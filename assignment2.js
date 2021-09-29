55303c9dbb4a75ecddbb2201294c4f4ed3a36ddc
Shinnosuke-Takahashi <shinnosuketakahashisemail@gmail.com>
2021-09-29 19:57:43 -0400

    grabValues + test

--- assignment2.js
+++ assignment2.js
@@ -136,4 +136,10 @@ Object.grabKeys = function(x) {
 
 // VALUES //
 Object.grabValues = function(x) {
+    let output = [];
+    for (key in x) {
+        if (x === undefined) continue;
+        output.push(x[key]);
+    }
+    return output;
 };
